dda35e7bc229321e806e7bc4ee9d2c5fb88623f6
Jody Winters <jodywinters11@hotmail.com>
2021-03-23 12:08:11 -0400

    Working on Shopping project

--- virt-env/Hello.py
+++ virt-env/Hello.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def student():
-   return render_template('student.html')
+   return render_template('homepage.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
